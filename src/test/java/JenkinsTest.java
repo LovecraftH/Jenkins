756f78efc1784e3363eb5b042b540f4c4e586a88
Mikhail Agapov <tmnovosibirsk@gmail.com>
2025-03-16 21:32:32 +0700

    не показывает логи

--- src/test/java/JenkinsTest.java
+++ src/test/java/JenkinsTest.java
@@ -1,19 +1,18 @@
-
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-@Slf4j
 public class JenkinsTest {
-
+    private static final Logger logger = LoggerFactory.getLogger(JenkinsTest.class);
 
     @Test
     @Tag("UI")
     public void first() {
-        log.info("Тест номер 1 UI");
+        logger.info("Тест номер 1 UI");
         System.out.println("SYSTEM Тест номер 1 UI");
         Assertions.assertTrue(true);
 
@@ -22,7 +21,7 @@ public class JenkinsTest {
     @Test
     @Tag("UI")
     public void twoFalse() {
-        log.info("Тест номер 2 UI");
+        logger.info("Тест номер 2 UI");
         System.out.println("SYSTEM Тест номер 2 UI");
         Assertions.assertTrue(false);
     }
@@ -30,7 +29,7 @@ public class JenkinsTest {
     @Test
     @Tag("API")
     public void threeTrue() {
-        log.info("Тест номер 3 API");
+        logger.info("Тест номер 3 API");
         System.out.println("SYSTEM тест номер 3 API");
         Assertions.assertTrue(true);
     }
